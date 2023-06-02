9a9f8cd239b909a2c83b230b49f14623146ec85e
CserhatiDavid <71378332+CserhatiDavid@users.noreply.github.com>
2023-06-02 10:08:18 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+/*
+* File: app.js
+* Author: Cserhati David
+* Copyright: 2023, Cserhati David
+* Group: Szoft I-2 N
+* Date: 2023-02-23
+* Github: https://github.com/CserhatiDavid/
+* Licenc: GNU GPL
+* Nem vállalunk felelősséget a rossz jegyekért
+*/
